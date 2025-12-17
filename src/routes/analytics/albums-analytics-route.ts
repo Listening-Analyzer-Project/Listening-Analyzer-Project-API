9b568f2845fe9cf94c6816ddcb57ac94740b6560
alauby <alban.lauby@gmail.com>
2025-12-17 16:00:23 +0100

    validator for analytics endpoints

--- src/routes/analytics/albums-analytics-route.ts
+++ src/routes/analytics/albums-analytics-route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import AlbumAnalyticsController from '@/controllers/analytics/albums-analytics-controller';
+import AlbumsAnalyticsController from '@/controllers/analytics/albums-analytics-controller';
 import { wrapRoutes } from '@/utils';
+import { validateRequest } from '@/middleware';
+import { getAlbumsAnalyticsSchema } from '@/validators/analytics/albums-analytics-validator';
 
 const router = express.Router();
 
-router.get('/', AlbumAnalyticsController.getAlbumsAnalytics);
+router.get('/', validateRequest(getAlbumsAnalyticsSchema), AlbumsAnalyticsController.getAlbumsAnalytics);
 
 export default wrapRoutes(router);
---
+++ src/validators/analytics/tracks-analytics-validator.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const getTracksAnalyticsSchema = z.object({
+    query: z.object({
+        limit: z.coerce.number().int().nonnegative().optional(),
+        offset: z.coerce.number().int().nonnegative().optional(),
+        order_by: z.string().optional(),
+        order_dir: z.enum(['asc', 'desc']).optional(),
+        search: z.string().optional(),
+        user_ids: z.string().regex(/^(\d+,)*\d+$/, 'Format invalide (liste d\'ID séparés par des virgules)').optional(),
+    }),
+});
